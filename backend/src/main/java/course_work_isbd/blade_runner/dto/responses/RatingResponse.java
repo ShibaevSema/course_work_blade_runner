c2e40cb898129b52dd2d54136e1d4dc2c4a17599
Shibaev Sema <seemon.shibaev@yandex.ru>
2022-03-16 10:41:31 +0300

    final commit

--- backend/src/main/java/course_work_isbd/blade_runner/dto/responses/RatingResponse.java
+++ backend/src/main/java/course_work_isbd/blade_runner/dto/responses/RatingResponse.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class RatingResponse {
     private String name;
     private Integer rating;   // оценка очеловечивания
-    private Integer countPosAct = 0; // количество положительных действий
-    private Integer countNegAct = 0; // количество отрицательных действий
-    private Integer countGoodTest = 0; // количество пройденных тестов
-    private Integer countBadTest = 0; // количество отрицательных тестов
+    private Integer countPosAct; // количество положительных действий
+    private Integer countNegAct; // количество отрицательных действий
+    private Integer countGoodTest; // количество пройденных тестов
+    private Integer countBadTest; // количество отрицательных тестов
 }
